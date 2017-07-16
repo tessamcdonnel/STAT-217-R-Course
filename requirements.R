eca1750b64bc75ecabb73135baec0ead609e4eee
tessamcdonnel <tmcdonne@calpoly.edu>
2017-07-16 16:56:37 -0700

    added

---
+++ requirements.R
@@ -0,0 +1,5 @@
+devtools::install_version("randomForest", "4.6-12")
+devtools::install_version("ggplot2", "2.2.0")
+devtools::install_version("googleVis", "0.6.1")
+
+devtools::install_version("mosaic", "0.4.14")
