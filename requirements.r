7445ba993e3a851039ca66a86a330665e3e81b82
tessamcdonnel <tmcdonne@calpoly.edu>
2017-07-19 16:24:13 -0700

    added MASS package for anorexia data set

--- requirements.r
+++ requirements.r
@@ -1,3 +1,4 @@
 devtools::install_version("ggplot2", "2.2.0")
 devtools::install_version("mosaic", "0.14.4")
 devtools::install_version("HSAUR", "1.3-8")
+devtools::install_version("MASS", "7.3-47")
